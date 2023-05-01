f8c02bb7fcc9973ff821584ed8d937d3efd69d51
william-bispo <william.bispo.matos@gmail.com>
2023-05-01 17:17:28 -0300

    In progress

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
+import Card from '../UI/Card';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filterYear, setFilterYear] = useState('2023');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filterYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
